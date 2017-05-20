e5b1ddeaef0c52e1bab318aeea33f68dfaf4037b
VisualLinux <david_horvath67@yahoo.com>
2017-05-20 21:52:22 +0200

    Implement grid

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -1,16 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Snake
@@ -20,9 +14,81 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<SnakeRectangle> Rectangles;
         public MainWindow()
         {
+            Rectangles = new List<SnakeRectangle>();
             InitializeComponent();
+            AddRectangles();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Rectangles.Where(x=>x.Row==0&&x.Column<=3).ToList().ForEach(x=>x.Rectangle.Fill=Brushes.Black);
+        }
+
+        private void AddRectangles()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var column = (int)Math.Floor(i / 10d);
+                var row = i - 10 * column;
+                var rectangle = new SnakeRectangle(new Rectangle(),column,row);
+                Rectangles.Add(rectangle);
+                Grid.SetColumn(rectangle.Rectangle, column);
+                Grid.SetRow(rectangle.Rectangle, row);
+                grid.Children.Add(rectangle.Rectangle);
+            }
+
+        }
+
+        private void MoveSnake(Directions direction)
+        {
+            var filled = GetFilledRectangles();
+            switch (direction)
+            {
+                    case Directions.Down:
+                        filled[0].Rectangle.Fill = Brushes.Transparent;
+                        
+                    break;
+            }
+        }
+
+        private SnakeRectangle[] GetFilledRectangles()
+        {
+            return Rectangles.Where(x => Equals(x.Rectangle.Fill, Brushes.Black)).ToArray();
+        }
+    }
+
+    public enum Directions
+    {
+        Up,
+        Down,
+        Right,
+        Left
+    }
+
+    public class SnakeRectangle
+    {
+        public Rectangle Rectangle;
+        public int Row;
+        public int Column;
+
+        public SnakeRectangle()
+        {
+            
+        }
+
+        public SnakeRectangle(Rectangle rectangle)
+        {
+            Rectangle = rectangle;
+        }
+
+        public SnakeRectangle(Rectangle rectangle, int column, int row)
+        {
+            Rectangle = rectangle;
+            Row = row;
+            Column = column;
         }
     }
 }
